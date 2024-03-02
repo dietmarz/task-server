8c5f84e2c67d4d84654107d9d3ff077135ed5cce
Dietmar Zabel <dietmar.zabel@gmail.com>
2024-03-02 10:49:18 +0100

    initial

---
+++ src/test/java/de/centronic/task/server/TaskServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.centronic.task.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/de/centronic/task/server/TestTaskServerApplication.java
@@ -0,0 +1,23 @@
+package de.centronic.task.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestTaskServerApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(TaskServerApplication::main).with(TestTaskServerApplication.class).run(args);
+    }
+
+}
